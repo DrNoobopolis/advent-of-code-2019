63e5b27edd3e87361a2e009770949becb8df0a96
James Feldmayer <james.feldmayer01@gmail.com>
2020-12-01 14:34:28 +1100

    added git commands to README.md

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# D
+# Documentation
 
-d
+Intcode
+
+- 2
+- 5
+- 7
+- 9
+- 11
+- 13
+- 15
+- 17
+- 19
+- 21
+- 23
+- 25
+
+# Git commands
+
+- git clone https://github.com/DrNoobopolis/advent-of-code-2020.git
+- git status
+- git add .
+- git commit -m "updated day 10"
+- git push -u origin master
--- day2/main1.cpp
+++ day2/main1.cpp
@@ -3,7 +3,8 @@
 #include <fstream>
 #include <vector>
 
-int main() {
+int main()
+{
     std::string input;
 
     std::ifstream read;
@@ -15,65 +16,71 @@ int main() {
 
     int temp = 0;
 
-    for(auto i=0; i<input.size(); i++) { 
+    for (auto i = 0; i < input.size(); i++)
+    {
         //
 
-        if(input[i] == ',') {
+        if (input[i] == ',')
+        {
             container.push_back(temp);
             temp = 0;
             continue;
         }
 
         temp *= 10;
-        temp += input[i]-48;
+        temp += input[i] - 48;
     }
     container.push_back(temp);
 
     int noun, verb;
 
-    for(int y=0; y<100; y++) {
-        for(int x=0; x<100; x++) {
+    for (int y = 0; y < 100; y++)
+    {
+        for (int x = 0; x < 100; x++)
+        {
 
             std::vector<int> clone = container;
 
             clone[1] = x;
             clone[2] = y;
 
-            for(int i=0; i<clone.size(); i++) {
+            for (int i = 0; i < clone.size(); i++)
+            {
                 //std::cout << clone[i] << std::endl;
-                
-                switch (clone[i]) {
-                    case 1 :
-                        clone[clone[i+3]] = clone[clone[i+1]] + clone[clone[i+2]];
-                        i += 3;
-                        break;
-                    
-                    case 2 :
-                        clone[clone[i+3]] = clone[clone[i+1]] * clone[clone[i+2]];
-                        i += 3;
-                        break;
 
-                    case 99 :
-                        i = clone.size();
-                        break;
+                switch (clone[i])
+                {
+                case 1:
+                    clone[clone[i + 3]] = clone[clone[i + 1]] + clone[clone[i + 2]];
+                    i += 3;
+                    break;
 
-                    default:
-                        std::cerr << "error" << std::endl;
-                        break;
-                    }
+                case 2:
+                    clone[clone[i + 3]] = clone[clone[i + 1]] * clone[clone[i + 2]];
+                    i += 3;
+                    break;
+
+                case 99:
+                    i = clone.size();
+                    break;
+
+                default:
+                    std::cerr << "error" << std::endl;
+                    break;
+                }
 
                 //
             }
 
-            if(clone[0] == 19690720) {
+            if (clone[0] == 19690720)
+            {
                 noun = x;
                 verb = y;
             }
-
         }
     }
 
-    std::cout << 100*noun + verb << std::endl;
+    std::cout << 100 * noun + verb << std::endl;
 
     return 0;
 }
