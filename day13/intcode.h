e998b7312c471a62fc8739ebe3bdfb15a7a7a0b9
James Feldmayer <james.feldmayer01@gmail.com>
2020-12-01 16:26:48 +1100

    refactor

--- day13/intcode.h
+++ day13/intcode.h
@@ -1,20 +1,22 @@
 
 void printArray(std::vector<long> const &);
+
 void printVector(std::vector<long> const &);
 
-struct program {
-    private:
-        std::vector<long> intcode;
-        bool first = true;
-        std::vector<long> read();
-        long find(std::vector<long> const &, long, long);
-        long iterator = 0;        
-        long relative_base = 0;
+struct program
+{
+private:
+    std::vector<long> intcode;
+    bool first = true;
+    std::vector<long> read();
+    long find(std::vector<long> const &, long, long);
+    long iterator = 0;
+    long relative_base = 0;
 
-    public:   
-        long get_duration();
-        program();
-        void run(std::vector<int> &);
+public:
+    long get_duration();
+    program();
+    void run(std::vector<int> &);
 };
 
 const long duration = program().get_duration();
--- day13/main.cpp
+++ day13/main.cpp
@@ -4,67 +4,84 @@
 #include <vector>
 #include <math.h>
 #include <algorithm>
+#include <array>
 
 #include "intcode.h"
 
-int main() {
+int main()
+{
     program single;
 
     std::vector<int> pinball;
 
     single.run(pinball);
 
-    int max_x=0, max_y=0, counter=0;
-    for(auto element : pinball) {
-        if(counter == 0) {
-            if(element > max_x) {
+    int max_x = 0, max_y = 0, counter = 0;
+
+    for (auto element : pinball) //what is this about?
+    {
+        if (counter == 0)
+        {
+            if (element > max_x)
+            {
                 max_x = element;
             }
         }
-        else if(counter == 1) {
-            if(element > max_y) {
+        else if (counter == 1)
+        {
+            if (element > max_y)
+            {
                 max_y = element;
             }
         }
         counter++;
-        if(counter == 3) {
+        if (counter == 3)
+        {
             counter = 0;
         }
     }
 
-    int screen[max_y][max_x] = {0};
+    std::vector<std::vector<int>> screen(max_y);
+    std::fill(screen.begin(), screen.end(), std::vector<int>(max_x));
 
-    int ans = 0;
+    int ans = 0; // what is this calculating?
 
     int x, y;
     counter = 0;
-    for(auto element : pinball) {
-        if(counter == 0) {
+    for (auto element : pinball)
+    {
+        if (counter == 0)
+        {
             x = element;
         }
-        else if(counter == 1) {
+        else if (counter == 1)
+        {
             y = element;
         }
-        else if(counter == 2) {
+        else if (counter == 2)
+        {
             screen[y][x] = element;
-            
-            if(element == 2) {
+
+            if (element == 2)
+            {
                 ans++;
             }
         }
-        counter++;  
-        if(counter == 3) {
+        counter++;
+        if (counter == 3)
+        {
             counter = 0;
-        }      
+        }
     }
 
-    for(int yi = 0; yi < max_y; yi++) {
-        for(int xi = 0; xi < max_x; xi++) {
-            std::cout << screen[yi][xi] << " ";         
+    for (int yi = 0; yi < max_y; yi++)
+    {
+        for (int xi = 0; xi < max_x; xi++)
+        {
+            std::cout << screen[yi][xi] << " ";
         }
         std::cout << std::endl;
     }
 
     std::cout << ans << std::endl;
-
 }
